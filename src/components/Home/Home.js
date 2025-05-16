7afe0d5636a057abf3a71f74e08c149c0847ea7f
Lee Gellie <43454850+LeeGellie417@users.noreply.github.com>
2025-05-16 12:26:15 -0400

    checking CI/CD

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Tom Robert Anderson</strong>
+                <strong className="main-name"> Tom Robert Anderson !!!</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
